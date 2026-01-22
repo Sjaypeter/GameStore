55e0fd3cb174251c01e1bdf8a1a04d3723775088
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-22 16:10:52 +0100

    feat:adding data model

---
+++ GameStore.Api/GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Games>();
+
+    public DbSet<Genre> Genres => Set<Genre>();
+
+
+}
--- GameStore.Api/GameStore.Api/Program.cs
+++ GameStore.Api/GameStore.Api/Program.cs
@@ -4,6 +4,9 @@ using GameStore.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
