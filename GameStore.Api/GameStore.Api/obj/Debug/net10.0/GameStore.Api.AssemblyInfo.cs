d62b9429ef28f24f7908be88f7447ae84b8a7611
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-22 14:19:42 +0100

    feat:add folder for endpoints and created function for endpoints in program.cs

--- GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6618bf3e1341920b27397555eef6fe74ff4f83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10542af0fc28fadd9f887412bc9590bb151e99c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
