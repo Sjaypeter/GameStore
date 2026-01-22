2f9a167f8faa37af2750574892eb96a797b21a7f
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-22 13:52:52 +0100

    feat:add content type to put request

--- GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e02c882785012e1dc424d43fda712db2900c320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16ddbb3a1a7c9a411d2dbd23c017713e26aba5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
