3af92629afdb1d9e65e88953e021c3686bcc8e3b
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-23 13:13:55 +0100

    feat:add Readme file

--- GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10542af0fc28fadd9f887412bc9590bb151e99c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00784f8773c672ad68f15753c38a6cc486f7d304")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
